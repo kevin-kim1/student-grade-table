0e9dfd3131001b967510d3ee10422fd4af8be119
Kevin Kim <kevinnhkim@live.com>
2020-03-20 14:48:48 -0700

    Added App class to app.js

---
+++ components/app.js
@@ -0,0 +1,22 @@
+class App {
+
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess(this);
+  }
+  handleGetGradesError(error) {
+    console.log("error");
+  }
+  handleGetGradesSuccess(success) {
+    console.log("success");
+  }
+  getGrades() {
+    $.jax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError
+
+    });
+  }
+}
