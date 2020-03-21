d30638a579062902dc5feb5798282696f55a41f4
Kevin Kim <kevinnhkim@live.com>
2020-03-20 18:17:51 -0700

    Rendering the grades completed

--- main.js
+++ main.js
@@ -2,8 +2,3 @@ var table = document.getElementById("grade-table");
 var gradeTable = new GradeTable(table);
 var app = new App(gradeTable);
 app.start();
-// var table = document.getElementById("grade-table");
-// var gradeTable = new GradeTable(table);
-
-// var getGrades = new App(gradeTable);
-// getGrades.start();
