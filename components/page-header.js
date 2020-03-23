2608be4ece5b696545f4588fe69ad110488650c1
Kevin Kim <kevinnhkim@live.com>
2020-03-22 18:05:57 -0700

    made query selector for header and instatiated PageHeader class

--- components/page-header.js
+++ components/page-header.js
@@ -2,4 +2,8 @@ class PageHeader {
   constructor(headerElement) {
     this.headerElement = headerElement;
   }
+
+  updateAverage(newAverage) {
+    console.log(newAverage);
+  }
 }
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var table = document.getElementById("grade-table");
 var gradeTable = new GradeTable(table);
+var header = document.getElementById("student-table-header");
+var tableHeader =  new PageHeader(header);
 var app = new App(gradeTable);
 app.start();
