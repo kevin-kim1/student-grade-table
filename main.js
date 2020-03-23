a2e86ca030ca7b7250a32b1ff5a1517056a995d9
Kevin Kim <kevinnhkim@live.com>
2020-03-22 18:08:29 -0700

    in main js, added second variable PageHeader to variable app

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var table = document.getElementById("grade-table");
 var gradeTable = new GradeTable(table);
 var header = document.getElementById("student-table-header");
 var tableHeader =  new PageHeader(header);
-var app = new App(gradeTable);
+var app = new App(gradeTable, tableHeader);
 app.start();
