54036efc289c0cb88c6bcf5b3bd07afabe08487d
Kevin Kim <kevinnhkim@live.com>
2020-03-22 23:02:56 -0700

    completed implentatation of handleSumbit method

--- components/grade-form.js
+++ components/grade-form.js
@@ -6,6 +6,14 @@ class GradeForm {
 
   handleSubmit(event) {
     event.preventDefault();
+    var formData = new FormData(event);
+    var name = formData.get("name");
+    var course = formData.get("course");
+    var grade = formData.get("grade");
+    this.name.createGrade(name);
+    this.course.createGrade(course);
+    this.grade.createGrade(grade);
+
   }
 
   constructor(formElement) {
